e8bb6cd5349f4f7db74a5b0fda18d18aeb0673a7
allison <vargasallison535@gmail.com>
2024-05-30 10:08:11 -0500

    cambio

--- aleatorio.rb
+++ aleatorio.rb
@@ -4,6 +4,7 @@ numero_usuario =gets.chomp.to_i
 numero_aleatorio = rand (0..10)
 
 puts "el numero aleatorio es : #{numero_aleatorio}"
+
 if numero_usuario == numero_aleatorio
     puts "felicidades has ganado"
 else 
